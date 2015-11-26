75e2856289b81ebe144103d845f1a091c3887bd4
Clara Bennett <csojinb@gmail.com>
2015-11-26 14:03:05 -0500

    Run updraft as a command-line tool
    Just uses positional arguments for module and api names, rather
    than gunicorn-like colon notation

---
+++ updraft/__main__.py
@@ -0,0 +1,32 @@
+import argparse
+
+from dev_server import run_dev_server
+
+
+def main():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+
+    module_name = args.pop('module')
+    api_name = args.pop('api')
+
+    exec('from {} import {} as api'.format(module_name, api_name))
+    run_dev_server(api, **args)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('module')
+    parser.add_argument('api')
+    parser.add_argument('-H', '--hostname', default='127.0.0.1')
+    parser.add_argument('-p', '--port', default=5000)
+    parser.add_argument('-d', '--use-debugger',
+                        action='store_true', default=False)
+    parser.add_argument('--no-reload', action='store_false', default=True,
+                        dest='use_reloader')
+
+    return parser
+
+
+if __name__ == '__main__':
+    main()
