b48450ea51ef2a236efde991715fde6b48bb5c5c
Clara Bennett <csojinb@gmail.com>
2016-06-02 11:53:23 -0700

    Fix setup.py requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='updraft',
     version='0.0.1',
     packages=find_packages(exclude=['tests']),
-    requires=['falcon'],
+    install_requires=['falcon'],
     entry_points={
         'console_scripts': [
             'updraft = updraft.__main__:main'
