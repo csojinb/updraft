58fdf22a8f9cb73a17660f3c6f49677282e08b44
Clara Bennett <csojinb@gmail.com>
2016-06-23 18:52:44 -0400

    Remove watchdog reloader (not using)

--- updraft/_reloader.py
+++ updraft/_reloader.py
@@ -80,20 +80,35 @@ def _find_common_roots(paths):
 
 
 class ReloaderLoop(object):
-    name = None
+    name = 'stat'
 
-    # monkeypatched by testsuite. wrapping with `staticmethod` is required in
+    # wrapping with `staticmethod` is required in
     # case time.sleep has been replaced by a non-c function (e.g. by
     # `eventlet.monkey_patch`) before we get here
     _sleep = staticmethod(time.sleep)
 
     def __init__(self, extra_files=None, interval=1):
-        self.extra_files = set(os.path.abspath(x)
-                               for x in extra_files or ())
+        self.extra_files = set(
+            os.path.abspath(x) for x in extra_files or ())
         self.interval = interval
 
     def run(self):
-        pass
+        mtimes = {}
+        while 1:
+            for filename in chain(_iter_module_files(),
+                                  self.extra_files):
+                try:
+                    mtime = os.stat(filename).st_mtime
+                except OSError:
+                    continue
+
+                old_time = mtimes.get(filename)
+                if old_time is None:
+                    mtimes[filename] = mtime
+                    continue
+                elif mtime > old_time:
+                    self.trigger_reload(filename)
+            self._sleep(self.interval)
 
     def restart_with_reloader(self):
         """Spawn a new Python interpreter with the same arguments as this one,
@@ -126,118 +141,10 @@ class ReloaderLoop(object):
         _log('info', ' * Detected change in %r, reloading' % filename)
 
 
-class StatReloaderLoop(ReloaderLoop):
-    name = 'stat'
-
-    def run(self):
-        mtimes = {}
-        while 1:
-            for filename in chain(_iter_module_files(),
-                                  self.extra_files):
-                try:
-                    mtime = os.stat(filename).st_mtime
-                except OSError:
-                    continue
-
-                old_time = mtimes.get(filename)
-                if old_time is None:
-                    mtimes[filename] = mtime
-                    continue
-                elif mtime > old_time:
-                    self.trigger_reload(filename)
-            self._sleep(self.interval)
-
-
-class WatchdogReloaderLoop(ReloaderLoop):
-
-    def __init__(self, *args, **kwargs):
-        ReloaderLoop.__init__(self, *args, **kwargs)
-        from watchdog.observers import Observer
-        from watchdog.events import FileSystemEventHandler
-        self.observable_paths = set()
-
-        def _check_modification(filename):
-            if filename in self.extra_files:
-                self.trigger_reload(filename)
-            dirname = os.path.dirname(filename)
-            if dirname.startswith(tuple(self.observable_paths)):
-                if filename.endswith(('.pyc', '.pyo')):
-                    self.trigger_reload(filename[:-1])
-                elif filename.endswith('.py'):
-                    self.trigger_reload(filename)
-
-        class _CustomHandler(FileSystemEventHandler):
-
-            def on_created(self, event):
-                _check_modification(event.src_path)
-
-            def on_modified(self, event):
-                _check_modification(event.src_path)
-
-            def on_moved(self, event):
-                _check_modification(event.src_path)
-                _check_modification(event.dest_path)
-
-            def on_deleted(self, event):
-                _check_modification(event.src_path)
-
-        reloader_name = Observer.__name__.lower()
-        if reloader_name.endswith('observer'):
-            reloader_name = reloader_name[:-8]
-        reloader_name += ' reloader'
-
-        self.name = reloader_name
-
-        self.observer_class = Observer
-        self.event_handler = _CustomHandler()
-        self.should_reload = False
-
-    def trigger_reload(self, filename):
-        # This is called inside an event handler, which means throwing
-        # SystemExit has no effect.
-        # https://github.com/gorakhargosh/watchdog/issues/294
-        self.should_reload = True
-        self.log_reload(filename)
-
-    def run(self):
-        watches = {}
-        observer = self.observer_class()
-        observer.start()
-
-        while not self.should_reload:
-            to_delete = set(watches)
-            paths = _find_observable_paths(self.extra_files)
-            for path in paths:
-                if path not in watches:
-                    try:
-                        watches[path] = observer.schedule(
-                            self.event_handler, path, recursive=True)
-                    except OSError as e:
-                        message = str(e)
-
-                        if message != "Path is not a directory":
-                            # Log the exception
-                            _log('error', message)
-
-                        # Clear this path from list of watches We don't want
-                        # the same error message showing again in the next
-                        # iteration.
-                        watches[path] = None
-                to_delete.discard(path)
-            for path in to_delete:
-                watch = watches.pop(path, None)
-                if watch is not None:
-                    observer.unschedule(watch)
-            self.observable_paths = paths
-            self._sleep(self.interval)
-
-        sys.exit(3)
-
-
 def run_with_reloader(main_func, extra_files=None, interval=1):
     """Run the given function in an independent python interpreter."""
     import signal
-    reloader = StatReloaderLoop(extra_files, interval)
+    reloader = ReloaderLoop(extra_files, interval)
     signal.signal(signal.SIGTERM, lambda *args: sys.exit(0))
     try:
         if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
